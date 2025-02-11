ad9553459f42bd1cc5c77223ffcbfe573df99c33
ArthurWillameBr <arthurwillame2005@gmail.com>
2025-02-11 09:33:47 -0300

    feat: add validation to prevent organization creation with duplicate email and ensure password hashing

--- src/use-cases/create-org.spec.ts
+++ src/use-cases/create-org.spec.ts
@@ -2,6 +2,8 @@ import { beforeEach, describe, expect, it } from "vitest";
 import { CreateOrgUseCase } from "./create-org";
 import { InMemoryOrgsRepository } from "@/repositories/in-memory/in-memory-orgs-repository";
 import { makeOrg } from "@/tests/factories/make-org";
+import { OrgAlreadyExistsError } from "./errors/org-already-exists-error";
+import { compare } from "bcryptjs";
 
 describe("CreateOrg", () => {
   let orgsRepository: InMemoryOrgsRepository;
@@ -17,5 +19,28 @@ describe("CreateOrg", () => {
 
     expect(orgsRepository.items).toHaveLength(1);
     expect(org.id).toEqual(expect.any(String));
+  });
+
+  it("should not be able to create a new org with an already used email", async () => {
+    const email = "test@gmail.com";
+
+    const org = makeOrg({ email });
+
+    await orgsRepository.create(org);
+
+    await expect(sut.execute(org)).rejects.toBeInstanceOf(
+      OrgAlreadyExistsError
+    );
+  });
+
+  it("should be able to create a new org with a password hash", async () => {
+    const password = "123456";
+
+    const { org } = await sut.execute(makeOrg({ password }));
+
+    expect(await compare(password, org.password)).toBe(true);
+    expect(await compare(password, orgsRepository.items[0].password)).toBe(
+      true
+    );
   });
 });
